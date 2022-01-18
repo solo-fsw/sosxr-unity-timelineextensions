e46b172c5a5e48d38eaaf38cf0eccd3270ed4404
mrstruijk <mrstruijk@gmail.com>
2022-01-18 11:43:02 +0100

    Added Looper

---
+++ CustomPlayables/Looper/Editor/LoopBreakerBaseDrawer.cs
@@ -0,0 +1,30 @@
+using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(LoopBreakerBase), true)] // That boolean allows us to use this Custom Editor for child classes too
+public class LoopBreakerBaseDrawer : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+
+		var loopBreaker = (LoopBreakerBase) target;
+
+		GUILayout.Space(10f);
+		GUILayout.BeginVertical(EditorStyles.helpBox);
+		if (GUILayout.Button(nameof (loopBreaker.GoToStart)))
+		{
+			loopBreaker.GoToStart();
+		}
+		else if (GUILayout.Button(nameof (loopBreaker.BreakLoop)))
+		{
+			loopBreaker.BreakLoop();
+		}
+		else if (GUILayout.Button(nameof (loopBreaker.GoToEnd)))
+		{
+			loopBreaker.GoToEnd();
+		}
+		GUILayout.EndVertical();
+	}
+}
---
+++ CustomPlayables/Looper/LoopBreaker.cs
@@ -0,0 +1,7 @@
+/// <summary>
+/// This is an empty implementation of the base
+/// </summary>
+public class LoopBreaker : LoopBreakerBase
+{
+
+}
